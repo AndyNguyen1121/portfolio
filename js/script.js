351a4690c49a1de96d07868ff53bfc4c4786d940
AndyNguyen1121 <andyhoangnguyen2005@gmail.com>
2026-08-26 01:29:47 -0700

    finish spar k rumble

--- js/script.js
+++ js/script.js
@@ -48,3 +48,17 @@ sections.forEach(section => {
 if (sidebarLinks.length > 0) {
   setActiveLink(sidebarLinks[0]);
 }
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+document.querySelectorAll(".zoomable").forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.classList.add("active");
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.classList.remove("active");
+});
